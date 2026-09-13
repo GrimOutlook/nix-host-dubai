00872cf2c942af74630e0734fe04f1b09a35a086
Dominic Grimaldi <dev@grimoutlook.dev>
2026-09-12 22:47:26 -0500

    fix(home-assistant): use mse camera playback

--- modules/home-assistant/lovelace/helpers.nix
+++ modules/home-assistant/lovelace/helpers.nix
@@ -10,7 +10,7 @@ rec {
         {
           inherit camera_entity;
           live_provider = "go2rtc";
-          go2rtc.modes = [ "mjpeg" ];
+          go2rtc.modes = [ "mse" ];
         }
       ];
     };
